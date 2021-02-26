9049ee46aa3d272852e7ba1dbd1aa1344c8231f2
jacques <jacksovoessi@hotmail.com>
2021-02-26 16:18:21 +0100

    Chap03 Exo12

---
+++ Chap03/Exo12/SimpleInterest.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+class SimpleInterest {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int numOfYears;
+        float principal, rateOfInterest, investment;
+
+        System.out.print("Enter the principal: ");
+        principal = sc.nextFloat();
+        System.out.print("Enter the rate of interest: ");
+        rateOfInterest = sc.nextFloat();
+        System.out.print("Enter the number of years: ");
+        numOfYears = sc.nextInt();
+
+        investment = principal * (1 + rateOfInterest/100 *numOfYears);
+        investment = Math.round(investment * 10) /100f;
+
+        System.out.print("After " +numOfYears+ " years at " +
+            rateOfInterest+"%, the investment will be worth $"+ investment);    
+
+    }
+}
